5213e71a008bfeace5174da3dd1955c8f921dbc8
Timurdjumaniyazov <dtstimdjumaniyazov@gmail.com>
2025-12-20 00:37:54 +0500

    exercise 3.10 - update port

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -132,7 +132,7 @@ app.post('/api/persons', (request, response) => {
 })
 
 
-const PORT = 3001
+const PORT = 3001 || process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
