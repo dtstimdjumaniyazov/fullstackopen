c85ce9715f59b1aad95e456a6d42084e7578e102
Timurdjumaniyazov <dtstimdjumaniyazov@gmail.com>
2025-12-23 01:39:02 +0500

    exercise 3.12

---
+++ part3/phonebook/mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://dtstimdjumaniyazov_db_user:${password}@cluster0.ighrj1c.mongodb.net/phoneBook?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url, { family: 4 })
+
+const phoneBookSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const PhoneBook = mongoose.model('Phonebook', phoneBookSchema)
+
+const phonebook = new PhoneBook({
+    name: process.argv[3],
+    number: process.argv[4],
+})
+
+if (process.argv.length > 3) {
+    phonebook.save().then(result => {
+        console.log(`added ${result.name} number ${result.number} to phonebook`)
+        mongoose.connection.close()
+    })
+}
+
+if (process.argv.length === 3)
+PhoneBook.find({}).then(result => {
+    console.log('phonebook:')
+    result.forEach((phonebook) => {
+        console.log(`${phonebook.name} ${phonebook.number}`)
+    })
+    mongoose.connection.close()
+})
