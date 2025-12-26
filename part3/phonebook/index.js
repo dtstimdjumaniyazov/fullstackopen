f0ba01eaeddb7c14234ced0aa46a4e28dac76f93
Timurdjumaniyazov <dtstimdjumaniyazov@gmail.com>
2025-12-26 18:49:52 +0500

    exercise 3.13-3.14

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,30 +1,32 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
+const PhoneBook = require('./models/phoneBook')
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+// let persons = [
+//     { 
+//       "id": "1",
+//       "name": "Arto Hellas", 
+//       "number": "040-123456"
+//     },
+//     { 
+//       "id": "2",
+//       "name": "Ada Lovelace", 
+//       "number": "39-44-5323523"
+//     },
+//     { 
+//       "id": "3",
+//       "name": "Dan Abramov", 
+//       "number": "12-43-234345"
+//     },
+//     { 
+//       "id": "4",
+//       "name": "Mary Poppendieck", 
+//       "number": "39-23-6423122"
+//     }
+// ]
 
 const generatedId = () => {
     const maxId = persons.length > 0
@@ -61,7 +63,9 @@ app.use(express.static('dist'))
 
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    PhoneBook.find({}).then(results => {
+        response.json(results)
+    })
 })
 
 
@@ -73,67 +77,85 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const person = persons.find((person) => person.id === id)
-    if (person) {
-        return response.status(200).json(person)
-    } else {
-        return response.status(404).json({
-            error: `no such person with ${id}`
-        })
-    }  
+    // const id = request.params.id
+    // const person = persons.find((person) => person.id === id)
+    // if (person) {
+    //     return response.status(200).json(person)
+    // } else {
+    //     return response.status(404).json({
+    //         error: `no such person with ${id}`
+    //     })
+    // }  
+    PhoneBook.findById(request.params.id).then(result => {
+        response.json(result)
+    })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const personExists = persons.find((person) => person.id === id)
+    // const id = request.params.id
+    // const personExists = persons.find((person) => person.id === id)
     
-    if (!personExists) {
-        return response.status(404).json({
-            error: `Person with ID: ${id} doesn't exists`
-        })
-    }
+    // if (!personExists) {
+    //     return response.status(404).json({
+    //         error: `Person with ID: ${id} doesn't exists`
+    //     })
+    // }
 
-    persons = persons.filter((person) => person.id !== id)
-    response.status(204).end()
+    // persons = persons.filter((person) => person.id !== id)
+    // response.status(204).end()
+    PhoneBook.deleteOne({_id: request.params.id}).then(result => {
+        response.json(result)
+    })
 })
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
-    personName = persons.find((person) => person.name === body.name)
-    // console.log(personName)
+    // personName = persons.find((person) => person.name === body.name)
+    // // console.log(personName)
 
-    if (personName) {
-        return response.status(405).json({
-            error: 'name must be unique'
-        })
-    }
+    // if (personName) {
+    //     return response.status(405).json({
+    //         error: 'name must be unique'
+    //     })
+    // }
 
-    if (!body.name) {
-        return response.status(400).json({
-            error: 'name missing'
-        })
-    }
+    // if (!body.name) {
+    //     return response.status(400).json({
+    //         error: 'name missing'
+    //     })
+    // }
 
-    if (!body.number) {
-        return response.status(400).json({
-            error: 'number missing'
-        })
-    }
+    // if (!body.number) {
+    //     return response.status(400).json({
+    //         error: 'number missing'
+    //     })
+    // }
 
-    const person = {
-        id: generatedId(),
-        name: body.name,
-        number: body.number,
-    }
-    persons = persons.concat(person)
+    // const person = {
+    //     id: generatedId(),
+    //     name: body.name,
+    //     number: body.number,
+    // }
+    // persons = persons.concat(person)
     
-    response.status(201).json(person)
-    request.loggerDetails
+    // response.status(201).json(person)
+    // request.loggerDetails
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'name or number is missing'
+        })
+    }
+    const phonebook = new PhoneBook({
+        name: body.name,
+        number: body.number
+    })
+    phonebook.save().then(result => {
+        response.json(result)
+    })
 })
 
 
-const PORT = process.env.PORT || 3001 
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
