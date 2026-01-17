206f6f493f137c2a71d785979688f1dc4ef32aeb
Timurdjumaniyazov <dtstimdjumaniyazov@gmail.com>
2026-01-18 04:51:27 +0500

    4.5-4.7.

--- part4/Blog List/utils/list_helper.js
+++ part4/Blog List/utils/list_helper.js
@@ -1,3 +1,5 @@
+var _ = require('lodash');
+
 const dummy = (blogs) => {
     return 1
 }
@@ -13,7 +15,52 @@ const totalLikes = (arr) => {
     return sum
 }
 
+const favoriteBlog = (arr) => {
+    if (!arr || arr.length === 0) {
+        return null
+    }
+    const likesArr = arr.map(a => a?.likes)
+    return arr.find(a => a.likes === Math.max(...likesArr))
+}
+
+const mostBlogs = (blogs) => {
+    const authors = _.countBy(blogs, 'author')
+    
+    const newObject = Object.entries(authors).map(([author, blogs]) => {
+        return {author, blogs}
+    })
+    const maxNumbers = _.maxBy(newObject, 'blogs')
+    // console.log(maxNumbers)
+    return maxNumbers
+}
+
+const mostLikes = (blogs) => {
+    const authors = _.groupBy(blogs, 'author')
+    // console.log(authors)
+    const totalLikes = _.mapValues(authors, function(o) {
+        return o
+            .map(x => {
+                return x.likes
+            })
+            .reduce((sum, n) => {
+                return sum + n
+            }, 0)
+        }
+    )
+    
+    const newObject = Object.entries(totalLikes).map(([author, likes]) => {
+        return {author, likes}
+    })
+    
+    const maxNumbers = _.maxBy(newObject, 'likes')
+    // console.log(maxNumbers)
+    return maxNumbers
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog,
+    mostBlogs,
+    mostLikes,
 }
