1b4cfbaa3db30bed4863518a26d1ec67e49f95e9
Timurdjumaniyazov <dtstimdjumaniyazov@gmail.com>
2026-04-11 22:37:11 +0500

    ex 6.2-6.6

---
+++ part6/zustand-anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,26 @@
+import { useAnecdoteActions } from "../store"
+
+const AnecdoteForm = () => {
+    const { addAnecdote } = useAnecdoteActions()
+
+    const addNewAnecdote = (e) => {
+        e.preventDefault()
+        const content = e.target.content.value
+        addAnecdote(content)
+        e.target.reset()
+    }
+
+    return (
+        <div>
+            <h2>create new</h2>
+            <form onSubmit={addNewAnecdote}>
+                <div>
+                <input name="content"/>
+                </div>
+                <button>create</button>
+            </form>
+        </div>
+    )
+}
+
+export default AnecdoteForm
